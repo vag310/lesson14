3495a98debbe37154c8b85924f354127757e5835
vag310 <44344193+vag310@users.noreply.github.com>
2020-03-04 21:01:13 +0300

    feat: add galp-sass file

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const {src, dest, watch} = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+ 
+
+function bs() {
+    serveSass();
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    watch("./*.html").on('change', browserSync.reload)
+    watch("./sass/**/*.sass", serveSass)
+    watch("./js/*.js").on('change', browserSync.reload)
+};
+
+function serveSass() {
+    return src("./sass/*.scss")
+    .pipe(sass())
+    .pipe(dest("./css"))
+    .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
