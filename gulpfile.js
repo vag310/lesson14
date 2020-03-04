9027b5da17f48722e2e13838996855afc756bd37
vag310 <44344193+vag310@users.noreply.github.com>
2020-03-04 21:14:51 +0300

    fix: serve sass

--- gulpfile.js
+++ gulpfile.js
@@ -10,13 +10,13 @@ function bs() {
             baseDir: "./"
         }
     });
-    watch("./*.html").on('change', browserSync.reload)
-    watch("./sass/**/*.sass", serveSass)
-    watch("./js/*.js").on('change', browserSync.reload)
+    watch("./*.html").on('change', browserSync.reload);
+    watch("./sass/**/*.sass", serveSass);
+    watch("./js/*.js").on('change', browserSync.reload);
 };
 
 function serveSass() {
-    return src("./sass/*.scss")
+    return src("./sass/*.sass")
     .pipe(sass())
     .pipe(dest("./css"))
     .pipe(browserSync.stream());
